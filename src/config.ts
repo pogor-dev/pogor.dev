139776c8c755d2ba3c11dab543448ac0624c2333
Victor Pogor <victor.pogor@outlook.com>
2024-05-26 01:49:23 +0000

    chore: project config

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://pogor.dev/",
+  author: "Victor Pogor",
+  desc: "Personal blog about software development.",
+  title: "pogor.dev",
+  ogImage: "pogor-dev.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "en",
+  langTag: ["en-EN"],
 } as const;
 
 export const LOGO_IMAGE = {
