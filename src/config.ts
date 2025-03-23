1313f263a95fe7d39a6c4dfa5a267b27cee5ee72
Victor Pogor <victor.pogor@outlook.com>
2025-03-23 22:22:09 +1000

    changes

--- src/config.ts
+++ src/config.ts
@@ -20,8 +20,3 @@ export const SITE = {
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "UTC", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
-
-export const ANALYTICS = {
-  enable: true,
-  measurementId: "G-VS87PQP0YV",
-} as const;
