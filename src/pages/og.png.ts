99504c908be2e3c4d3a31e8e6f2c46c12a13baa9
Victor Pogor <victor.pogor@outlook.com>
2025-11-03 13:59:14 +0000

    fix(og): fixed generateOgImageForSite contract

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,9 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
+export const GET: APIRoute = async () => {
+  const png = await generateOgImageForSite();
+  return new Response(new Uint8Array(png), {
     headers: { "Content-Type": "image/png" },
   });
+};
--- src/pages/posts/[...slug]/index.png.ts
+++ src/pages/posts/[...slug]/index.png.ts
@@ -27,10 +27,8 @@ export const GET: APIRoute = async ({ props }) => {
     });
   }
 
-  return new Response(
-    await generateOgImageForPost(props as CollectionEntry<"blog">),
-    {
-      headers: { "Content-Type": "image/png" },
-    }
-  );
+  const png = await generateOgImageForPost(props as CollectionEntry<"blog">);
+  return new Response(new Uint8Array(png), {
+    headers: { "Content-Type": "image/png" },
+  });
 };
