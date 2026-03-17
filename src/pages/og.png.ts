13ff581f304685baf1ea28f829946f582e1ab05f
Victor Pogor <victor.pogor@outlook.com>
2026-03-17 23:32:41 +1000

    project cleanup

--- src/pages/og.png.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import { generateOgImageForSite } from "@/utils/generateOgImages";
-
-export const GET: APIRoute = async () => {
-  const png = await generateOgImageForSite();
-  return new Response(new Uint8Array(png), {
-    headers: { "Content-Type": "image/png" },
-  });
-};
