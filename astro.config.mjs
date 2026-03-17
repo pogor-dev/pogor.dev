2b408761eefd582ab521e2c8e329cf4ac5bc9b4a
Victor Pogor <victor.pogor@outlook.com>
2026-03-18 00:08:42 +1000

    feat: add configuration files for formatting and font management

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      name: "Inter",
+      cssVariable: "--font-inter",
+      provider: fontProviders.fontsource(),
+    },
+    {
+      name: "Montserrat",
+      cssVariable: "--font-montserrat",
+      provider: fontProviders.fontsource(),
+    },
+  ],
+});
